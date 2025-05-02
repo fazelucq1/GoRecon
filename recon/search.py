95fc550eb09f88bb53761142c635d9885c22e4ea
fazelucq <119872173+fazelucq1@users.noreply.github.com>
2025-05-02 18:28:32 +0200

    Update search.py

--- recon/search.py
+++ recon/search.py
@@ -1,10 +1,6 @@
 import shodan
 from config import SHODAN_API_KEY
 
-def shodan_search(query: str) -> dict:
+def shodan_search(service: str) -> dict:
     api = shodan.Shodan(SHODAN_API_KEY)
-    try:
-        result = api.search(query)
-        return result
-    except shodan.APIError as e:
-        raise RuntimeError(f"Errore Shodan: {e}")
+    return api.search(service)
