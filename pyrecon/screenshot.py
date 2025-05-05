508992bfe34643710a992e570852d92fdb5d5c2d
fazelucq <119872173+fazelucq1@users.noreply.github.com>
2025-05-05 10:05:10 +0200

    Update screenshot.py

--- pyrecon/screenshot.py
+++ pyrecon/screenshot.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
-import os
+
 
 def capture_screenshot(url: str) -> str:
     options = Options()
@@ -9,7 +9,7 @@ def capture_screenshot(url: str) -> str:
     driver = webdriver.Chrome(options=options)
     driver.set_window_size(1920, 1080)
     driver.get(url)
-    filename = f"screenshots/{url.replace('http://', '').replace('/', '_')}_{int(time.time())}.png"
+    filename = f"screenshots/{url.replace('http://', '').replace(':', '_').replace('/', '_')}_{int(time.time())}.png"
     driver.save_screenshot(filename)
     driver.quit()
     return filename
